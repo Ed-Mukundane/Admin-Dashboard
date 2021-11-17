511ff52047bc83231f2630cdf9bb438b8682a8bc
Ed-Mukundane <aedgar990@gmail.com>
2021-11-17 11:36:38 +0300

    Second Commit

--- src/components/widgetLarge/WidgetLarge.jsx
+++ src/components/widgetLarge/WidgetLarge.jsx
@@ -1,9 +1,81 @@
+/** @format */
+
 import "./WidgetLarge.css";
 
 export default function WidgetLarge() {
-    return (
-        <div className="widgetLarge">
-            Large
-        </div>
-    )
+	const Button = ({ type }) => {
+		return <button className={"widgetLargeButton " + type}>{type}</button>;
+	};
+	return (
+		<div className="widgetLarge">
+			<h3 className="widgetLargeTitle">Latest Transactions</h3>
+			<table className="widgetLargeTable">
+				<tr className="widgetLargeTr">
+					<th className="widgerLargeTh">Customer</th>
+					<th className="widgerLargeTh">Date</th>
+					<th className="widgerLargeTh">Amount</th>
+					<th className="widgerLargeTh">Status</th>
+				</tr>
+				<tr className="widgetLargeTr">
+					<td className="widgetLargeUser">
+						<img src="./assets/user2.jpg" alt="" className="widgetLargeImg" />
+						<span className="widgetLargeUsername">Bandese Chris</span>
+					</td>
+					<td className="widgetLargeDate">2 Jun 2021</td>
+					<td className="widgetLargeAmount">Ugx.672,000</td>
+					<td className="widgetLargeStatus">
+						<Button type="Approved" />
+					</td>
+				</tr>
+
+				<tr className="widgetLargeTr">
+					<td className="widgetLargeUser">
+						<img src="./assets/user2.jpg" alt="" className="widgetLargeImg" />
+						<span className="widgetLargeUsername">Bandese Chris</span>
+					</td>
+					<td className="widgetLargeDate">2 Jun 2021</td>
+					<td className="widgetLargeAmount">Ugx.672,000</td>
+					<td className="widgetLargeStatus">
+						<Button type="Declined" />
+					</td>
+				</tr>
+
+				<tr className="widgetLargeTr">
+					<td className="widgetLargeUser">
+						<img src="./assets/user2.jpg" alt="" className="widgetLargeImg" />
+						<span className="widgetLargeUsername">Bandese Chris</span>
+					</td>
+					<td className="widgetLargeDate">2 Jun 2021</td>
+					<td className="widgetLargeAmount">Ugx.672,000</td>
+					<td className="widgetLargeStatus">
+						<Button type="Pending" />
+					</td>
+				</tr>
+
+				<tr className="widgetLargeTr">
+					<td className="widgetLargeUser">
+						<img src="./assets/user2.jpg" alt="" className="widgetLargeImg" />
+						<span className="widgetLargeUsername">Bandese Chris</span>
+					</td>
+					<td className="widgetLargeDate">2 Jun 2021</td>
+					<td className="widgetLargeAmount">Ugx.672,000</td>
+					<td className="widgetLargeStatus">
+						<Button type="Approved" />
+					</td>
+				</tr>
+
+				<tr className="widgetLargeTr">
+					<td className="widgetLargeUser">
+						<img src="./assets/user2.jpg" alt="" className="widgetLargeImg" />
+						<span className="widgetLargeUsername">Bandese Chris</span>
+					</td>
+					<td className="widgetLargeDate">2 Jun 2021</td>
+					<td className="widgetLargeAmount">Ugx.672,000</td>
+					<td className="widgetLargeStatus">
+						<Button type="Approved" />
+					</td>
+				</tr>
+			</table>
+		</div>
+	);
 }
--- src/components/widgetSmall/WidgetSmall.jsx
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -1,9 +1,71 @@
 import "./WidgetSmall.css";
+import { Visibility } from "@material-ui/icons";
 
 export default function WidgetSmall() {
     return (
-        <div className="widgetSmall">
-            Small
-        </div>
-    )
+			<div className="widgetSmall">
+				<span className="widgetSmallTitle">New Join Member</span>
+				<ul className="widgetSmallList">
+					<li className="widgetSmallListItem">
+						<img src="./assets/user1.jpg" alt="" className="widgetSmallImg" />
+						<div className="widgetSmallUser">
+							<span className="widgetSmallUsername">Edgar Ampiire</span>
+							<span className="widgetSmallUserJobTitle">Web Developer</span>
+						</div>
+						<button className="widgetSmallButton">
+							<Visibility className="widgetSmallButtonIcon" />
+							Display
+						</button>
+					</li>
+
+					<li className="widgetSmallListItem">
+						<img src="./assets/user1.jpg" alt="" className="widgetSmallImg" />
+						<div className="widgetSmallUser">
+							<span className="widgetSmallUsername">Edgar Ampiire</span>
+							<span className="widgetSmallUserJobTitle">Web Developer</span>
+						</div>
+						<button className="widgetSmallButton">
+							<Visibility className="widgetSmallButtonIcon" />
+							Display
+						</button>
+					</li>
+
+					<li className="widgetSmallListItem">
+						<img src="./assets/user1.jpg" alt="" className="widgetSmallImg" />
+						<div className="widgetSmallUser">
+							<span className="widgetSmallUsername">Edgar Ampiire</span>
+							<span className="widgetSmallUserJobTitle">Web Developer</span>
+						</div>
+						<button className="widgetSmallButton">
+							<Visibility className="widgetSmallButtonIcon" />
+							Display
+						</button>
+					</li>
+
+					<li className="widgetSmallListItem">
+						<img src="./assets/user1.jpg" alt="" className="widgetSmallImg" />
+						<div className="widgetSmallUser">
+							<span className="widgetSmallUsername">Edgar Ampiire</span>
+							<span className="widgetSmallUserJobTitle">Web Developer</span>
+						</div>
+						<button className="widgetSmallButton">
+							<Visibility className="widgetSmallButtonIcon" />
+							Display
+						</button>
+					</li>
+
+					<li className="widgetSmallListItem">
+						<img src="./assets/user1.jpg" alt="" className="widgetSmallImg" />
+						<div className="widgetSmallUser">
+							<span className="widgetSmallUsername">Edgar Ampiire</span>
+							<span className="widgetSmallUserJobTitle">Web Developer</span>
+						</div>
+						<button className="widgetSmallButton">
+							<Visibility className="widgetSmallButtonIcon" />
+							Display
+						</button>
+					</li>
+				</ul>
+			</div>
+		);
 }
